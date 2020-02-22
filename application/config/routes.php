74d4763161a5e39c60417e14cee700e70496efdb
Amina <ba.amina.01@gmail.com>
2020-02-22 08:08:16 -0500

    Crud todolist

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['todolist'] = 'todolist';
+$route['todolist/add'] = 'todolist/add';
+$route['todolist/view/:id'] = 'todolist/view';
+$route['todolist/edit/:id'] = 'todolist/edit';
+$route['todolist/delete/:id'] = 'todolist/delete';
